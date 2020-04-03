0a5d70a8bc2409e5f91175ba6c58b60bbe4d73f1
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-04-02 21:36:52 -0700

    maths

--- lean-1.lean
+++ lean-1.lean
@@ -2,7 +2,6 @@ constant m : nat
 -- m is a natural number
 constant n : nat 
 -- n is a constant number
-constants x y : nat
 constant f : nat → nat 
 constant f'' : ℕ -> ℕ 
 constant p : nat × nat
@@ -11,9 +10,22 @@ constant g : nat → nat → nat
 constant g' : nat → (nat → nat)
 constant h : nat × nat → nat
 constant F : (nat → nat) → nat
+constant x : nat
+constant y : nat
+-- #check fun x : nat, x + y
+constants α β  : Type
+constants a1 a2 : α
+constants b1 b2 : β
 
+constant fn : α → α
+constant gn : α → α
+constant hn : α → β → α
+constant pn : α → α → bool 
+#check fun x : α, fn x
+#check λ x : α, fn x -- α -> α
+#check λ x : α, fn (fn x)
+#check λ x : α, hn x b1 
 
-constants b1 b2 : bool 
 -- b1 and b2 are booleans
 
 #check m
@@ -21,8 +33,8 @@ constants b1 b2 : bool
 #check n + 0
 #check m * (n + 0)
 #check b1
-#check b1 && b2 
-#check b1 || b2
+-- #check b1 && b2 
+-- #check b1 || b2
 #check tt
 #check f m 
 #check g m
