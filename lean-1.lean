816d9f5d8153c60bdd264e76dae90537774e0526
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-04-04 19:53:58 -0700

    Dear god..

--- lean-1.lean
+++ lean-1.lean
@@ -13,9 +13,7 @@ constant F : (nat → nat) → nat
 constant x : nat
 constant y : nat
 -- #check fun x : nat, x + y
-constants α β  : Type
-constants a1 a2 : α
-constants b1 b2 : β
+
 
 
 -- b1 and b2 are booleans
@@ -42,11 +40,15 @@ constant ξ : Type u
 constant YU : (ξ → ξ)
 #check YU  
 
-constant fn : α → α
-constant gn : α → α
-constant hn : α → β → α
-constant pn : α → α → bool 
-#check fun x : α, fn x
+-- constant fn : α → α
+-- constant gn : α → α
+-- constant hn : α → β → α
+-- constant pn : α → α → bool 
+-- #check fun x : α, fn x
+
+#check fun x : nat, x + 5
+#check λ x : nat, x + 5
+
 -- x is the name of function that recieves α, and a function that recieves and
 -- returns a. x => fn(x), with x being (the variable) bound to α in the context,
 -- making α the free variable?
@@ -57,7 +59,24 @@ constant pn : α → α → bool
 -- fun x maps any value x, to the value t, which thusly depends on x.
 -- In mathematics it is said "let f be the function that maps a natural number x to x + 5"
 -- This can be written as λ x : nat, x + r5
--- Or fun x : nat -> nat without the λ-abstraction
-#check λ x : α, fn x -- α -> α
-#check λ x : α, fn (fn x)
-#check λ x : α, hn x b1 
+-- Or fun x : nat -> nat without the λ-abstraction?
+
+constants α β  : Type
+constants a1 a2 : α
+constants b1 b2 : β
+constant f : α -> α
+constant g : α -> β
+constant h : α -> β -> α
+constant p : α -> α -> bool 
+#check fun x : α, f x -- λ (x : α), f x : α → α
+#check λ x : α, f x -- λ (x : α), f x : α → α
+#check λ x : α, f (f x) -- λ (x : α), f (f x) : α → α
+#check λ x : α, h x b1 -- λ (x : α), h x b1 : α → α
+#check λ y : β, h a1 y -- λ (y : β), h a1 y : β → α
+#check λ x : α, p (f (f x)) (h (f a1) b2) -- λ (x : α), p (f (f x)) (h (f a1) b2) : α → bool
+#check λ x : α , λ y : β, h (f x) y -- λ (x : α) (y : β), h (f x) y : α → β → α
+#check λ (x : α) (y : β), h (f x) y -- λ (x : α) (y : β), h (f x) y : α → β → α
+
+-- #check λ x : α, fn x -- α -> α
+-- #check λ x : α, fn (fn x)
+-- #check λ x : α, hn x b1 
